88240962c187d098dfd70fe05e2950a88dbd0f20
Rafael134-hub <rafael.barbosa.moreira24@gmail.com>
2025-08-28 09:29:32 -0300

    porta arrumada

--- main.py
+++ main.py
@@ -1,10 +1,15 @@
 from fastapi import FastAPI 
 from core.configs import settings
 from api.v1.api import api_router
+import os
+import uvicorn
 
 app = FastAPI(title='Answer Requests API - FastAPI SQL Alchemy')
 app.include_router(api_router, prefix=settings.API_V1_STR)
 
 if __name__ == '__main__':
-    import uvicorn
-    uvicorn.run("main:app", host="127.0.0.1", port=8000, log_level='info', reload=True)
+    
+
+    port = int(os.environ.get("PORT", 8000))  # Pega a porta do Render ou usa 8000 localmente
+    uvicorn.run("main:app", host="0.0.0.0", port=port, log_level='info', reload=True)
+
